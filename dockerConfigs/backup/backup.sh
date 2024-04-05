b4339a8e7b67719229d568f85514e774d8d9e5f7
Ammar amine <amineammar20@icloud.com>
2024-04-06 00:07:11 +0200

    fix main

--- dockerConfigs/backup/backup.sh
+++
@@ -1,6 +0,0 @@
-
-# Clé de chiffrement (remplacez 'votre_mot_de_passe' par votre propre phrase secrète)
-export BORG_PASSPHRASE='F2i2023@grp2'
-
-# Exécute la commande de sauvegarde
-/usr/bin/borg create /backup::sauvegarde__$(date +%Y-%m-%d_%H-%M-%S) ~/docker
